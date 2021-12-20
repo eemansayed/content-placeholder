a0189d278f57be16a0f1d9f702117d9354751f3c
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-20 09:44:16 +0000

    add function called getData

--- script.js
+++ script.js
@@ -6,4 +6,19 @@ const name = document.getElementById("name");
 const date = document.getElementById("date");
 
 const animated_bgs = document.querySelectorAll(".animated-bg");
-const animated_bgs_texts = document.querySelectorAll(".animated-bg-text");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+setTimeout(getData, 2500);
+
+function getData() {
+  header.innerHTML = <img src="images/tiny-pink-flowers.jpg" alt="" />;
+  title.innerHTML = "Adipisicing odit eligendi hic eius.";
+  excerpt.innerHTML =
+    "Elit corrupti ratione ad soluta earum Odio sequi corporis voluptatum";
+  profile_img.innerHTML = <img src="images/stripy-cat.jpg" alt="" />;
+  name.innerHTML = "Mariam Green";
+  date.innerHTML = "Dec 20, 2021";
+
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
+}
